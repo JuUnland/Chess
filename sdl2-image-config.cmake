f48cfd87905e2f66a48faf46904b2cdda29d2aa4
Julian Unland <juunland@uni-osnabrueck.de>
2021-10-13 12:04:36 +0200

    added config files

---
+++ sdl2-image-config.cmake
@@ -0,0 +1,10 @@
+set(SDL2_IMAGE_DIRS "${CMAKE_CURRENT_LIST_DIR}/include")
+
+# Support both 32 and 64 bit builds
+if (${CMAKE_SIZEOF_VOID_P} MATCHES 8)
+  set(SDL2_IMAGE_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x64/SDL2_image.lib")
+else ()
+  set(SDL2_IMAGE_LIBRARIES "${CMAKE_CURRENT_LIST_DIR}/lib/x86/SDL2_image.lib")
+endif ()
+
+string(STRIP "${SDL2_IMAGE_LIBRARIES}" SDL2_IMAGE_LIBRARIES)
